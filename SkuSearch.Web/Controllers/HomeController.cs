197a966f428d40ba8805b373f6a7ea9def2a5975
Yuriy <yu_chubey@ukr.net>
2014-12-05 23:12:36 +0200

    extract geolocation from view

--- SkuSearch.Web/Controllers/HomeController.cs
+++ SkuSearch.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SkuSearch.DataAccess;
+using SkuSearch.Web.Models;
 
 namespace SkuSearch.Web.Controllers
 {
@@ -24,6 +25,40 @@ namespace SkuSearch.Web.Controllers
             var tuple = new Tuple<IEnumerable<SkuSearch.DataAccess.MultilanguageProperty>, IEnumerable<SkuSearch.DataAccess.Product>>(freshProducts, products);
 
             return View(tuple);            
+        }
+
+        [HttpPost]
+        public ActionResult StoreLocator(string address)
+        {
+            // Make sure we have an address
+            if (string.IsNullOrEmpty(address))
+                return View();
+
+            var results = GoogleMapsAPIHelpersCS.GetGeocodingSearchResults(address);
+
+            var resultCount = results.Elements("result").Count();
+
+            if (resultCount == 0)
+            {
+                // No matching address found!
+                ViewData["No Results"] = true;
+                return View();
+            }
+            else if (resultCount == 1)
+            {
+                // Got back exactly one result, show it!
+                return RedirectToAction("StoreLocatorResults", new { Address = results.Element("result").Element("formatted_address").Value });
+            }
+            else
+            {
+                // Got back multiple results - We need to ask the user which address they mean to use...
+                var matches = from result in results.Elements("result")
+                              let formatted_address = result.Element("formatted_address").Value
+                              select formatted_address;
+
+                ViewData["Matches"] = matches;
+                return View();
+            }
         }
     }
 }
